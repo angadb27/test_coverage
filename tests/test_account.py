b57ed9173cd79bdf020220ecd6eb92ec6edb9524
Angad Bhatti <61568322+AB27-source@users.noreply.github.com>
2023-09-26 22:48:31 -0700

    Modified test_account.py for 100% coverage of account.py

--- tests/test_account.py
+++ tests/test_account.py
@@ -6,6 +6,7 @@ from random import randrange
 from unittest import TestCase
 from models import db
 from models.account import Account, DataValidationError
+from models import app
 
 ACCOUNT_DATA = {}
 
@@ -14,6 +15,7 @@ class TestAccountModel(TestCase):
 
     @classmethod
     def setUpClass(cls):
+        app.app_context().push()
         """ Load data needed by tests """
         db.create_all()  # make our sqlalchemy tables
         global ACCOUNT_DATA
@@ -53,3 +55,63 @@ class TestAccountModel(TestCase):
         account.create()
         self.assertEqual(len(Account.all()), 1)
 
+    def test_repr(self):
+        """Test the representation of an account"""
+        account = Account()
+        account.name = "Foo"
+        self.assertEqual(str(account), "<Account 'Foo'>")
+
+    def test_to_dict(self):
+        """ Test account to dict """
+        data = ACCOUNT_DATA[self.rand] # get a random account
+        account = Account(**data)
+        result = account.to_dict()
+        self.assertEqual(account.name, result["name"])
+        self.assertEqual(account.email, result["email"])
+        self.assertEqual(account.phone_number, result["phone_number"])
+        self.assertEqual(account.disabled, result["disabled"])
+        self.assertEqual(account.date_joined, result["date_joined"])
+    
+    def test_from_dict(self):
+        """ Test account from dict """
+        data = {
+            "name": "Test Name",
+            "email": "test@example.com",
+            "phone_number": "1234567890",
+            "disabled": False,
+            "date_joined": "2023-09-26T12:00:00" 
+        }
+        account = Account() 
+        account.from_dict(data) 
+        self.assertEqual(account.name, data["name"])
+        self.assertEqual(account.email, data["email"])
+        self.assertEqual(account.phone_number, data["phone_number"])
+        self.assertEqual(account.disabled, data["disabled"])
+        self.assertEqual(account.date_joined, data["date_joined"])
+
+    
+    def test_update(self):
+        """ Test successful update of an account """
+        data = ACCOUNT_DATA[self.rand]  # get a random account
+        account = Account(**data)
+        account.create()  
+        account.name = "Updated Name"
+        account.update()
+        updated_account = Account.find(account.id)
+        self.assertEqual(updated_account.name, "Updated Name")
+
+    def test_update_empty_id(self):
+        """ Test update of an account with no ID """
+        data = ACCOUNT_DATA[self.rand]  # get a random account
+        account = Account(**data)
+        with self.assertRaises(DataValidationError):
+            account.update()
+            
+    def test_delete(self):
+        """ Test deletion of an account """
+        data = ACCOUNT_DATA[self.rand]  # get a random account
+        account = Account(**data)
+        account.create() 
+        account_id = account.id
+        account.delete()
+        self.assertIsNone(Account.find(account_id))
